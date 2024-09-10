2ffdd74c41eaf4b77619eb0505a87b616a33902b
4259c <158604908+4259c@users.noreply.github.com>
2024-09-10 09:17:42 +0530

    Updated environment configuration and code

--- Adhar.js
+++ Adhar.js
@@ -31,6 +31,7 @@ app.use("/uploads", express.static("uploads"));
 // Create a MySQL connection using environment variables
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
